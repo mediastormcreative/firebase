33f106a7589c3bd74ce1b40b3daa9256082ce5cd
Damien Storm <damien@mediastormcreative.com>
2017-06-30 21:49:01 -0700

    Added filter method.
    Add a filter method enabling client-side filtering by value. Method is a boolean, returning a true/false depending on whether the filter is successful.

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -17,5 +17,14 @@ export class HomePage {
   ionViewDidLoad() {
     console.log(this.dinosaurs);
   }
+  
+  filter(dinosaur : any, value) : boolean{
+     // Return true if I want that specified height
+     // e.g. show only dinosaurs with a height of 4
+     if (dinosaur.height == value){
+       return false;
+     }
+     return true; 
+  }
 
 }
