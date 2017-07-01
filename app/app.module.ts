ad2738adc7bd866bc45318038cb0498350dd629a
Damien Storm <damien@mediastormcreative.com>
2017-06-30 20:55:30 -0700

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+export const firebaseConfig = {
+   apiKey: "AIzaSyDmJIJbkiCblDHi2eIWSw-YVxhMu7yu0Rw",
+    authDomain: "fir-test-27a1d.firebaseapp.com",
+    databaseURL: "https://fir-test-27a1d.firebaseio.com",
+    projectId: "fir-test-27a1d",
+    storageBucket: "fir-test-27a1d.appspot.com",
+    messagingSenderId: "974124840995"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ pages/home/home.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  dinosaurs: FirebaseListObservable<any[]>;
+
+  constructor(public navCtrl: NavController,  private afDB: AngularFireDatabase) {
+    this.dinosaurs = afDB.list('/');
+  }
+
+  ionViewDidLoad() {
+    console.log(this.dinosaurs);
+  }
+
+}
