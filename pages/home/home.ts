8e217eea86c73aeb2997a546a3a4377180ba531e
Damien Storm <damien@mediastormcreative.com>
2017-07-01 19:06:55 -0700

    Uploaded home.ts file
    Added the ability to order results by child.

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -11,11 +11,24 @@ export class HomePage {
   dinosaurs: FirebaseListObservable<any[]>;
 
   constructor(public navCtrl: NavController,  private afDB: AngularFireDatabase) {
-    this.dinosaurs = afDB.list('/');
+    this.dinosaurs = afDB.list('/', {
+      query: {
+        orderByChild: 'height'
+      }
+    })
   }
 
   ionViewDidLoad() {
     console.log(this.dinosaurs);
+  }
+
+  filter(target, value) : boolean{
+     // Return true if I want that specified height
+     // e.g. show only dinosaurs with a height of 4
+     if (target.height == value){
+       return false;
+     }
+     return true; 
   }
 
 }
